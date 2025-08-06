39aba239f017fdace9671a04b8a98f6b0318cfd2
SadiaAnjum120 <sadiaanjum53654@gmail.com>
2025-08-06 08:05:20 -0700

    Initial portfolio commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const form = document.querySelector("form");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thanks! Your message has been sent.");
+  form.reset();
+});
